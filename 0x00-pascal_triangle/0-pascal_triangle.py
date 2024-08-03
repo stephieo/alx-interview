6e83f522b1e9f40b737124abc06ea5001e8fa79f
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-03 05:07:40 +0100

    pascals

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Create a Pascal's Triangle .
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        list: A list of lists.
+        Each inner list is a row in theTriangle.
+        Empty list if n <= 0 or not an integer.
+    """
+
+
+def pascal_triangle(n):
+    if not isinstance(n, int) or n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle

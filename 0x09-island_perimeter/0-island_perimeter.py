88ef7107be78a8c4aea4380c44b687386a0c0b35
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-03 04:21:52 +0100

    validation

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """
+    Calculate and return the perimeter of the island described in grid.
+    """
+    if not grid:
+        return 0
+    
+   

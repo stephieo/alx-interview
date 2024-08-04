4639aa8f1701df9010f73e21651fe302a266a3c4
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-04 04:58:45 +0100

    nquens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,28 +4,55 @@
 
 import sys
 
-def solve_nqueens(row, column):
-    solution = [[]]
-    for queen in range(row):
-        solution = place_queen(queen, column, solution)
-    return solution
+def is_valid_position(pos, occupied_pos):
+    """Check if a position is valid for placing a queen"""
+    for i in range(len(occupied_pos)):
+        if (
+            occupied_pos[i] == pos or
+            occupied_pos[i] - i == pos - len(occupied_pos) or
+            occupied_pos[i] + i == pos + len(occupied_pos)
+        ):
+            return False
+    return True
 
-def place_queen(queen, column, prev_solution):
-    safe_position = []
-    for array in prev_solution:
-        for x in range(column):
-            if is_safe(queen, x, array):
-                safe_position.append(array + [x])
-    return safe_position
+def place_queens(n, index, occupied_pos, solutions):
+    """Place queens on the board and collect valid positsions"""
+    if index == n:
+        solutions.append(occupied_pos[:])
+        return
 
-def is_safe(q, x, array):
-    if x in array:
-        return False
-    else:
-        return all(abs(array[column] - x) != q - column for column in range(q))
+    for i in range(n):
+        if is_valid_position(i, occupied_pos):
+            occupied_pos.append(i)
+            place_queens(n, index + 1, occupied_pos, solutions)
+            occupied_pos.pop()
+
+def solve_nqueens(n):
+    solutions = []
+    place_queens(n, 0, [], solutions)
+    return solutions
 
 def init():
+    """Initialize and validate input"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if sys.argv[1].isdigi
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return n
+
+def n_queens():
+    """Main function to solve and print the N queens solutions"""
+    n = init()
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(len(solution))])
+
+if __name__ == '__main__':
+    n_queens()
